fb9cd5af7d69ac69f41e8913855355ea5094f555
Noel Welsh <noel@noelwelsh.com>
2025-08-19 10:48:20 +0100

    Implement toList in terms of next

--- src/main/scala/projects/stream/Program.scala
+++ src/main/scala/projects/stream/Program.scala
@@ -3,5 +3,13 @@ package projects.stream
 trait Program[A] {
   protected def next: Option[A]
 
-  def toList: List[A]
+  def toList: List[A] = {
+    def loop(): List[A] =
+      this.next match {
+        case None    => Nil
+        case Some(a) => a :: loop()
+      }
+
+    loop()
+  }
 }
